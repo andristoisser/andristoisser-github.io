ae116d2bfff2d50adbff8ca63dfac78c7a1b920e
canceltheculture <aesthetic.other@gmail.com>
2023-11-13 17:40:17 +0100

    push

--- BSM/script.js
+++ BSM/script.js
@@ -9,22 +9,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const content = document.querySelector(".content");
     const enterCircle = document.getElementById("initial-overlay");
 
-
     // Initially hide the second video, header, and content
     video2.style.display = "none";
     header.style.opacity = 0;
     arrow.style.opacity = 0;
     content.style.opacity = 0;
-        document.body.style.overflow = "hidden"; // Enable scrolling
-
+    document.body.style.overflow = "hidden"; // Enable scrolling
 
     // Function to start video playback and hide the "Enter" circle
     function startVideo() {
         video1.pause();
         video1.style.opacity = 1; // Fade in the first video
         enterCircle.style.display = "none"; // Hide the "Enter" circle
-            document.body.style.overflow = "auto"; // Enable scrolling
-
+        document.body.style.overflow = "auto"; // Enable scrolling
     }
 
     // Add a click event listener to the document to start video on user click
@@ -52,5 +49,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // Smooth scrolling for internal anchor links
+    const internalLinks = document.querySelectorAll('a[href^="#"]');
+    internalLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
 
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const headerHeight = document.querySelector(".page-header").offsetHeight;
+                const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
 });
